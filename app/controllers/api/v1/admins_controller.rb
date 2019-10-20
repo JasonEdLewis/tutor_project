b0c3dcc862e33673623a25b5af6ec78f8db86106
jason <jasonedlewis@gmail.com>
2019-10-19 21:41:59 -0400

    slight refactoring of the auth with redux

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AdminsController < ApplicationController
 
     def index
         admins = Admin.all 
-        render json: admins
+        render json: admins, except: [ :created_at, :updated_at ]
     end
 
     def show
@@ -31,7 +31,7 @@ class Api::V1::AdminsController < ApplicationController
        decoded = JWT.decode token, 'sin-city', true, { algorithm: 'HS256' }
        admin_id = decoded[0]["admin_id"]
        admin = Admin.find(admin_id)
-       render json: admin
+       render json: admin, except: [:password_digest,:created_at, :updated_at]
 
     end
         private
--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,17 +2,18 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def login
-       
+        is_authenticated = nil
         admin = Admin.find_by(username: params[:username])
+        is_authenticated = admin.authenticate(params[:password])
        
-        if (admin && admin.authenticate(params[:password]))
+        if is_authenticated
             
             payload = { admin_id: admin.id }
             token = JWT.encode payload, 'sin-city' ,'HS256'
 
             render json: {token: token }
         else 
-            render json: {message: "Please enter valid username or password."}
+            render json: {message: "Please enter valid..."}
         end
     end
 
